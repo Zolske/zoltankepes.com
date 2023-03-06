88a1efb2b9bf0b89f08bb82fe236311987508d39
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2023-03-06 23:28:02 +0000

    add a dynamic changing description to dark/light mode button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 // import logo_title from "../assets/images/logo/zoltankepes_laptop_logo.png";
 import logo_title from "../assets/images/logo/zoltankepes_laptop_logo.svg";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [mode, setMode] = useState("dark");
+
   function handleClickMode() {
     const html = document.getElementsByTagName("html")[0];
     const lightMode = document.getElementById("lightMode");
@@ -11,6 +14,7 @@ export default function Navbar() {
     lightMode?.classList.toggle("hidden");
     darkMode?.classList.toggle("hidden");
     html.classList.toggle("dark");
+    setMode(mode === "dark" ? "light" : "dark");
   }
 
   function handleClickMenu() {
@@ -40,7 +44,6 @@ export default function Navbar() {
           <div>
             <div
               id="bar_1"
-              // className="w-8 h-1 bg-logo-blue mt-1 mb-1 ml-2 transition duration-300 ease-in-out group-hover:translate-y-2 group-hover:rotate-[-45deg]"
               className="w-8 h-0.5 bg-primary-1000 dark:bg-indigo-100 mt-1 mb-1 ml-1 transition duration-300 ease-in-out group-hover:bg-primary-500"
             ></div>
             <div
@@ -91,47 +94,52 @@ export default function Navbar() {
         id="sidepanel"
         className="w-[360px] fixed z-10 h-[250px] top-14 -left-[360px]  bg-neutral-300 overflow-x-hidden transition duration-1000 ease-in-out rounded-r-xl shadow-lg"
       >
-        <div className="bg-indigo-200 shadow-lg mb-1">
+        <div className="bg-indigo-200 shadow-lg">
           <h2 className="ml-2 text-indigo-600 bg-indigo-100 w-fit pl-1 pr-2 rounded-r-full">
             Settings
           </h2>
         </div>
         {/* dark / light mode button */}
-        <div className="cursor-pointer w-fit pl-3 pr-3 ml-4 bg-primary-200 h-8 rounded-full mt-1 mb-1 flex items-center shadow-md text-primary-1000 dark:text-indigo-100 dark:bg-primary-1000">
-          {/* dark mode button */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            id="darkMode"
-            className="w-6 h-6 text-primary-1000 hover:fill-primary-1000"
-            onClick={handleClickMode}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-            />
-          </svg>
-          {/* light mode button */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            id="lightMode"
-            className="w-6 h-6 text-indigo-100  hidden hover:fill-indigo-100"
-            onClick={handleClickMode}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-            />
-          </svg>
+        <div className="ml-2 w-fit bg-indigo-100 h-8 rounded-full mb-2 mt-1 flex">
+          <div className="cursor-pointer w-fit pl-3 pr-3 bg-primary-200 h-8 rounded-full flex items-center shadow-md text-primary-1000 dark:text-indigo-100 dark:bg-primary-1000">
+            {/* dark mode button */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              id="darkMode"
+              className="w-6 h-6 text-primary-1000 hover:fill-primary-1000"
+              onClick={handleClickMode}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+              />
+            </svg>
+            {/* light mode button */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              id="lightMode"
+              className="w-6 h-6 text-indigo-100  hidden hover:fill-indigo-100"
+              onClick={handleClickMode}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+              />
+            </svg>
+          </div>
+          <span className="self-center text-indigo-600 ml-2 mr-2 text-sm">
+            turn on {mode} mode
+          </span>
         </div>
         {/* pages section */}
         <div className="bg-indigo-200 shadow-lg">
