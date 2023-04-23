a96824e5ed0fda4842212ba411969ea4d6b26366
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2023-04-23 08:34:31 +0100

    test loading json from realtime db as array

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,7 +1,68 @@
-export default function notes() {
+import { ref, onValue } from "firebase/database";
+import { db } from "../../lib/firebase";
+import Image from "next/image";
+import icon_folder from "../../assets/images/icons/folder_page.png";
+import Link from "next/link";
+import { createJsonArray } from "../../lib/helperFunctions";
+import { rt_db_json_ref } from "../../lib/firebase";
+import { useState, useEffect } from "react";
+
+export async function getStaticProps() {
+  // get json from the realtime database
+  // https://firebase.google.com/docs/database/web/read-and-write#read_data
+  let data = "not ready";
+  onValue(rt_db_json_ref, (snapshot) => {
+    // data = snapshot.val();
+    data = createJsonArray(snapshot.val());
+  });
+
+  return {
+    props: {
+      json: data,
+    },
+  };
+}
+
+const myLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
+
+export default function Notes({ json }) {
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
+
+  // const note_folder = Object.keys(json);
+
   return (
     <>
-      <h1>Notes</h1>
+      <h1>{json}</h1>
+      {/* {note_folder.map((folder) => (
+        <div key={folder}>
+          <h3>
+            {json[folder].icon && (
+              <Image
+                loader={myLoader}
+                src={json[folder].icon}
+                alt={`${folder} icon`}
+                width={25}
+                height={25}
+                className="inline-block mr-2"
+              />
+            )}
+            {!json[folder].icon && (
+              <Image
+                src={icon_folder}
+                alt={`folder icon`}
+                width={25}
+                height={25}
+                className="inline-block mr-2"
+              />
+            )}
+            {folder}
+          </h3>
+        </div>
+      ))} */}
     </>
   );
 }
