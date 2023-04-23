ad2be178f4196d09623d583d10bc72fa126100e1
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2023-04-23 13:25:46 +0100

    test with updated getStaticProps()

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -7,52 +7,65 @@ import { rt_db_json_ref } from "../../lib/firebase";
 import { useState, useEffect } from "react";
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 
-// export async function getStaticProps() {
-// get json from the realtime database
-// https://firebase.google.com/docs/database/web/read-and-write#read_data
-//   let db_json,
-//     db_array = null;
-//   onValue(rt_db_json_ref, (snapshot) => {
-//     db_array = createJsonArray(snapshot.val());
-//     db_json = snapshot.val();
-//   });
+export async function getStaticProps() {
+  // get json from the realtime database
+  // https://firebase.google.com/docs/database/web/read-and-write#read_data
+  //   let db_json,
+  //     db_array = null;
+  //   onValue(rt_db_json_ref, (snapshot) => {
+  //     db_array = createJsonArray(snapshot.val());
+  //     db_json = snapshot.val();
+  //   });
+  let db_json,
+    db_array = null;
 
-//   return {
-//     props: {
-//       db_json: db_json,
-//       db_array: db_array,
-//     },
-//   };
-// }
+  // read data from firebase realtime database
+  onValue(rt_db_json_ref, (snapshot) => {
+    db_array = createJsonArray(snapshot.val())
+      ? createJsonArray(snapshot.val())
+      : null;
+    db_json = snapshot.val() ? snapshot.val() : null;
+  });
 
+  // check if data has been updated
+  function updateData() {
+    // if db_json && db_array are not "null"
+    if (db_json && db_array) {
+      // stop "setInterval"
+      clearInterval(myInterval);
+      // setWaitData("data has arrived");
+    }
+  }
+
+  // keep calling the "updateData" function
+  const myInterval = setInterval(updateData, 3000);
+
+  return {
+    props: {
+      db_json: db_json,
+      db_array: db_array,
+    },
+  };
+}
+
+// process images from url
 const myLoader = ({ src, width, quality }) => {
   return `${src}?w=${width}&q=${quality || 75}`;
 };
 
-export default function Notes() {
-  const [jsonArray, setJsonArray] = useState("wait for data to be loaded");
-  let db_json,
-    db_array = null;
-
-  onValue(rt_db_json_ref, (snapshot) => {
-    db_array = createJsonArray(snapshot.val());
-    db_json = snapshot.val();
-  });
-
-  function waitData() {
-    if (db_json && db_array) {
-      clearInterval(myInterval);
-      setJsonArray("data has arrived");
-      console.log(db_json);
-      console.log(db_array);
-    }
-  }
-
-  const myInterval = setInterval(waitData, 100);
-
+export default function Notes({ db_json, db_array }) {
+  // message shown to the user for the time the data is loading
+  const [waitData, setWaitData] = useState("wait for data to be loaded");
+  // console.log(db_array);
   return (
     <>
-      {!db_json && !db_array && <h1>{jsonArray}</h1>}
+      {!db_json && !db_array && <h1>{waitData}</h1>}
+      {db_array &&
+        db_array.map((folder) => (
+          <div key={folder[0]}>
+            <h3>{folder[0]}</h3>
+          </div>
+        ))}
       {/* {note_folder.map((folder) => (
         <div key={folder}>
           <h3>
